0c2636f613ba4cca6d451418d610392b90e37156
dadi-vardhan <vardhan.vishnu32@gmail.com>
2022-05-06 01:06:25 +0200

    code to generate embeddings and perform semanticsearch

---
+++ code/main.py
@@ -0,0 +1,34 @@
+import pickle as pkl
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import semantic_search
+import torch
+from sklearn.metrics.pairwise import cosine_similarity
+
+# read the pickle file and return the data
+def read_pickle(file_path:str):
+    with open(file_path, 'rb') as f:
+        data = pkl.load(f)
+    return data
+
+
+
+if __name__ == '__main__':
+    file_path = '../data/Output/RELISH/relish_embeddings.pkl'
+    data = read_pickle(file_path)
+    embedder = SentenceTransformer('dmis-lab/biobert-large-cased-v1.1')
+    queries = ['Investigating global trends in paraquat intoxication research from 1962 to 2015 using bibliometric analysis.',
+               'Environmental implications for disaster preparedness: lessons learnt from the Indian Ocean Tsunami.',
+               'Trends in and contributions to entrepreneurship research: a broad review of literature from 1996 to June 2012.']
+    embeds = data['embedding'].tolist()
+    for query in queries:
+        q = embedder.encode(query,convert_to_tensor=True, device=torch.device('cpu'))
+        similars = semantic_search(q, embeds, top_k=2)[0]
+        print("query:", query)
+        for similar in similars:
+            id = similar["corpus_id"]
+            score = similar["score"]
+            pmid = data.iloc[id]['PMID']
+            print(f'pmid:{pmid}|| score:{score}')
+        print("-"*50)
+    
+    
--- code/model.py
+++ code/model.py
@@ -1,19 +1,50 @@
+from zmq import device
 from sentence_transformers import SentenceTransformer
+from datamodule import CustomDataset
+import pickle as pkl
+from tqdm import tqdm
 import pandas as pd
-
-# get the first abstract
-df = pd.read_csv('../data/Input/RELISH/TSV/sample.tsv', sep='\t')
-
-abst1 = df.iloc[0]['abstract']
-abst2 = df.iloc[1]['abstract']
-query = [abst1+abst2]
-
-#tokenized_query = tokenizer(query,padding=True,return_tensors="pt")
+import torch
 
 
-#embed = model(tokenized_query)
+def generate_embeddings(data:CustomDataset, model:SentenceTransformer, 
+                            save_path:str,return_df:bool=False):
+    """ Generates the embeddings for the data and saves it in a pickle file
 
-sbert_model = SentenceTransformer('dmis-lab/biobert-large-cased-v1.1')
+    Args:
+        data (CustomDataset): CustomDataset object containing the data to be embedded
+        model (SentenceTransformer): SentenceTransformer object containing the model to be used
+        save_path (str): path to save the pickle file
+        return_df (bool, optional): if True, returns df with embeddings. Defaults to False.
 
-embed = sbert_model.encode(query)
-print(embed.shape)
+    Returns:
+        pd.DataFrame: pandas dataframe or None if return_df is False 
+    """
+    df = pd.DataFrame(columns=['PMID', 'embedding'])
+    print("*"*50) 
+    print('Generating embeddings...')
+    for pmid, text in tqdm(data):
+        embed = model.encode(text, convert_to_tensor=True, device=torch.device('cpu'))
+        df = df.append({'PMID': pmid, 'embedding': embed},ignore_index=True)
+    pkl.dump(df, open(save_path, 'wb'))
+    if return_df:
+        return df
+    
+   
+            
+if __name__ == '__main__':
+    MODEL_NAME = 'dmis-lab/biobert-large-cased-v1.1'
+    RELISH_DATA_PATH = '../data/Input/RELISH/TSV/sample.tsv'
+    TREC_DATA_PATH = '../data/Input/TREC/TSV/sample.tsv'
+    sbert_model = SentenceTransformer(MODEL_NAME)
+    relish_data = CustomDataset(RELISH_DATA_PATH)
+    trec_data = CustomDataset(TREC_DATA_PATH)
+    generate_embeddings(relish_data, sbert_model, '../data/Output/RELISH/relish_embeddings.pkl')
+    generate_embeddings(trec_data, sbert_model, '../data/Output/TREC/trec_embeddings.pkl')
+        
+
+
+    
+        
+
+
