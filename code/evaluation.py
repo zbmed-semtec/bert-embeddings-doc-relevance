282d6fb9e7e17c5a1297336d125f79bd06003c2d
dadi-vardhan <vardhan.vishnu32@gmail.com>
2022-08-12 18:25:51 +0200

    created precision matrix method

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,8 +1,11 @@
 
+
 import pandas as pd
 
 
 class PrecisionN:
+    """ Class for precision@n evaluation mehtod.
+    """
     def __init__(self, mat_path: str, tsv_path: str):
         """ Initializes the class with the matrix and tsv data paths
 
@@ -16,6 +19,13 @@ class PrecisionN:
         if ".pkl" in mat_path:
             self.mat_data = pd.read_pickle(mat_path)
         self.tsv_data = pd.read_csv(tsv_path, sep='\t')
+
+        self.n_at = [5,10,15,20,25,50]
+
+        #df for storing the precision@n values
+        self.pn_df = pd.DataFrame(columns=['PMID'])
+
+        self.relavance = 2
 
     def find_topn(self, n: int):
         """ Finds the top n precision values for each PMID
@@ -31,6 +41,7 @@ class PrecisionN:
 
         # handling matrix data
         indices = self.mat_data.index.tolist()
+        self.pn_df['PMID'] = indices
         headers = self.mat_data.columns.tolist()
 
         for index in indices:
@@ -46,7 +57,7 @@ class PrecisionN:
                     visited_pairs.append((pmid1,pmid2))
                     visited_pairs.append((pmid2,pmid1))
                     if self.tsv_data.loc[(self.tsv_data['PMID2'] == pmid2) &
-                                            (self.tsv_data['PMID1'] == pmid1)]['Rel-d2d'].values[0] == 2:
+                                            (self.tsv_data['PMID1'] == pmid1)]['Rel-d2d'].values[0] == self.relavance:
                         tp += 1
 
             try:
@@ -56,8 +67,18 @@ class PrecisionN:
 
         return list(zip(indices, precision_list))
 
-if __name__ == '__main__':
-    mat_path = "../trec_similarity_matrix_up.pkl"
-    trec_repurposed = '../trec_repurposed_matrix.tsv'
-    precision_n = PrecisionN(mat_path, trec_repurposed)
-    print(precision_n.find_topn(10))
+    def create_precision_matrix(self, save_path: str):
+        """ Creates the precision matrix for each n value and saves it to the
+            save_path
+
+        Args:
+            save_path (str): path to save the precision matrix
+        """
+
+        for n in self.n_at:
+            self.pn_df['p@'+str(n)] = [x[1] for x in self.find_topn(n)]
+
+        if ".pkl" in save_path:
+            self.pn_df.to_pickle(save_path)
+        if ".tsv" in save_path:
+            self.pn_df.to_csv(save_path, index=False)
