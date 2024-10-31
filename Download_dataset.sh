46e3df00e7b2369a20ee786557227aa00e54ebc8
rohitharavinder <rohitha0112@gmail.com>
2024-10-31 15:53:40 +0000

    add shell script to download datasets

---
+++ Download_dataset.sh
@@ -0,0 +1,18 @@
+pip install gdown
+cd data
+
+mkdir -p Split_Dataset/Data
+mkdir -p Split_Dataset/Ground_truth
+
+
+cd Split_Dataset/Data/
+gdown https://drive.google.com/uc?id=1L-4spewLWN7jMzA5uTiBNTHhPl926j64 -O relish_documents.tsv
+gdown https://drive.google.com/uc?id=1IgxzmajC09aUTz_awABPD_iEJ2s1VR0E -O test.npy
+gdown https://drive.google.com/uc?id=1xCoXFl0GTc5VbW7L-7joAL9ViudD0Nbh -O train.npy
+gdown https://drive.google.com/uc?id=1De4n5hf6kirLpMlILD2LTFTuwsgpVhKQ -O valid.npy
+
+
+cd ../Ground_truth
+gdown  https://drive.google.com/uc?id=1y9T41Faf9Oq2XOtWMD1U9fZe9OHLgLjv -O test.tsv
+gdown  https://drive.google.com/uc?id=1R1i74XWzILnlozwCfYItlequKIhMnHmB -O train.tsv
+gdown https://drive.google.com/uc?id=1ZupxAdTOWxmKPWlD5FOwEKbkdavt5Zxk -O valid.tsv
