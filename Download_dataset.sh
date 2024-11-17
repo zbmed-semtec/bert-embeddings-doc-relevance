bd25fd2403cffddfd1eacce8fc1adf09379debaa
rohitharavinder <rohitha0112@gmail.com>
2024-11-17 21:27:54 +0000

    📄 update dataset download script

--- Download_dataset.sh
+++ Download_dataset.sh
@@ -7,10 +7,9 @@ mkdir -p Split_Dataset/Ground_truth
 
 cd Split_Dataset/Data/
 gdown https://drive.google.com/uc?id=1L-4spewLWN7jMzA5uTiBNTHhPl926j64 -O relish_documents.tsv
-gdown https://drive.google.com/uc?id=1IgxzmajC09aUTz_awABPD_iEJ2s1VR0E -O test.npy
-gdown https://drive.google.com/uc?id=1xCoXFl0GTc5VbW7L-7joAL9ViudD0Nbh -O train.npy
-gdown https://drive.google.com/uc?id=1De4n5hf6kirLpMlILD2LTFTuwsgpVhKQ -O valid.npy
-
+gdown https://drive.google.com/file/uc?id=1-c-00aJd_ybSL17Jqs22nEKMPd1lz4TZ -O input_train_text_data.tsv
+gdown https://drive.google.com/file/uc?id=1uTORTQlIx-IOMIBcj4v1yLFX2XB-klwP -O input_test_text_data.tsv
+gdown https://drive.google.com/file/uc?id=1cbHgYKPtocC8Tu5daez34fN0M1_Be6AJ -O input_valid_text_data.tsv
 
 cd ../Ground_truth
 gdown  https://drive.google.com/uc?id=1y9T41Faf9Oq2XOtWMD1U9fZe9OHLgLjv -O test.tsv
